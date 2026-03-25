1f97acb4925cee2f5506180843af060b6c7527e4
Harshvardhan Pandit <me@harshp.com>
2026-03-25 18:43:06 +0000

    minutes 25 MAR 2026

--- code/minutes-generator/specific_minutes.sh
+++ code/minutes-generator/specific_minutes.sh
@@ -2,19 +2,20 @@
 
 # Check if argument is provided
 if [ -z "$1" ]; then
-  echo "Usage: '$0 <YY-MM-DD>' to generate minutes from 'meeting-20<year>-<month>-<date>.irc'"
+  echo "Usage: '$0 <YY-MM-DD> <irc|txt>' to generate minutes from 'meeting-20<year>-<month>-<date>.<irc|txt>'"
   exit 1
 fi
 
 time="$1"
-file="meeting-20${time}"
+file="meeting-20${time}.$2"
+filename="meeting-20${time}"
 
 # check if file exists
-if [ ! -f ./data/${file}.irc ]; then
-    echo "file ${file}.irc not found"
+if [ ! -f ./data/${file} ]; then
+    echo "file ${file} not found"
     exit -1
 fi
 
 # Replace with the desired command
-perl scribe.perl -implicitContinuations -final -emphasis ./data/${file}.irc > ../../meetings/${file}.html
-echo "Generated ../../meetings/${file}.html"
+perl scribe.perl -implicitContinuations -final -emphasis ./data/${file} > ../../meetings/${filename}.html
+echo "Generated ../../meetings/${filename}.html"
